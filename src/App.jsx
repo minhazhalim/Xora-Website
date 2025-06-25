8735857656f06c955bb3ba1b608e6bfef32a2702
Minhaz Halim Zim <mhzim93@gmail.com>
2025-06-25 15:48:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './sections/header.jsx';
+import Hero from './sections/hero.jsx';
+import Features from './sections/features.jsx';
+import Pricing from './sections/pricing.jsx';
+import Faq from './sections/faq.jsx';
+import Testimonials from './sections/testimonials.jsx';
+import Download from './sections/download.jsx';
+import Footer from './sections/footer.jsx';
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header/>
+      <Hero/>
+      <Features/>
+      <Pricing/>
+      <Faq/>
+      <Testimonials/>
+      <Download/>
+      <Footer/>
+    </main>
+  );
+};
+export default App;
